48f90f6b10799d2b573adf60b1d3129e3e3d82e6
Roberto Reyes <a00833852@tec.mx>
2022-05-13 12:35:01 -0500

    Agregamos comentarios

--- Tron2D.py
+++ Tron2D.py
@@ -8,12 +8,13 @@ Diego Rodriguez Orozco A00835032
 
 
 """
-
+'Importamos librerias necesarias'
 from time import sleep
 from turtle import *
 
 from freegames import square, vector
 
+'Declaramos vectores para movimiento y dirección de jugadores'
 p1xy = vector(-100, 0)
 p1aim = vector(4, 0)
 p1body = set()
@@ -22,13 +23,16 @@ p2xy = vector(100, 0)
 p2aim = vector(-4, 0)
 p2body = set()
 
-
+'Declaramos limites para el espacio jugable'
+'Nos sirve para que los jugadores pierdan si salen del área'
 def inside(head):
-    """Return True if head inside screen."""
-    return -200 < head.x < 200 and -200 < head.y < 200
+    """Sera verdadero siempre y cuando la cabeza se encuentre en espacio jugable"""
+    return -240 < head.x < 240 and -240 < head.y < 240
 
+
+'Cambiamos la entrada del menú al color equivalente para que turtle lo entienda'
 def convcolor(numcolor):
-    "Convertimos la opción a un color"
+
     if numcolor == '1':
         return "red"
     elif numcolor == '2':
@@ -42,17 +46,19 @@ def convcolor(numcolor):
     elif numcolor == '6':
         return "black"
     else:
+        'Como Easter Egg si no eliges un valor valido tu color sera gris'
         return "gray"
 
 
 def draw():
-    """Advance players and draw game."""
+    """Dibujamos el juego y movemos a los jugadores"""
     p1xy.move(p1aim)
     p1head = p1xy.copy()
 
     p2xy.move(p2aim)
     p2head = p2xy.copy()
 
+    'Establecemos condiciones de victoria por choque o salir del área'
     if not inside(p1head) or p1head in p2body:
         print('Player 1 wins!')
         return
@@ -61,15 +67,17 @@ def draw():
         print('Player 2 wins!')
         return
 
+    'Guardamos el recorrido de ambos jugadores'
     p1body.add(p1head)
     p2body.add(p2head)
 
+    'Dibujamos cada cuadro conforme se da el movimiento'
     square(p1xy.x, p1xy.y, 3, convcolor(p1color))
     square(p2xy.x, p2xy.y, 3, convcolor(p2color))
     update()
     ontimer(draw, 50)
 
-
+'Imprimimos menú de elección de colores'
 print("Rojo = 1         Azúl = 2        Verde = 3\n")
 print("Rosa = 4         Dorado = 5    Negro = 6\n")
 p1color = input("Jugador 1 elige un color: ")
@@ -78,9 +86,11 @@ print("Rosa = 4         Dorado = 5    Negro = 6\n")
 p2color = input("Jugador 2 elige un color: ")
 print("\n Empezando...\n")
 setup(420, 420, 370, 0)
-sleep(5)
+'Esperamos 5 segundos para evitar un inicio desagradable o apresurado'
+sleep(2)
 hideturtle()
 tracer(False)
+'Establecemos controles'
 listen()
 onkey(lambda: p1aim.rotate(90), 'a')
 onkey(lambda: p1aim.rotate(-90), 'd')
